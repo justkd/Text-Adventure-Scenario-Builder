a2fe953810e990b5465413f84bf77539e16c83e0
justKD <cady@notnatural.co>
2020-09-15 12:04:04 -0500

    Port original source

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+/**
+ * @file App.tsx
+ * @author justKD
+ * @fileoverview
+ * App entry point.
+ */
+
+import * as React from "react";
+
+import "./css/styles.css";
+
+import { Title } from "./tsx/Title";
+import { Diagram } from "./tsx/Diagram";
+import { Toolbar } from "./tsx/Toolbar";
+import { HelpDrawer } from "./tsx/HelpDrawer";
+
+import "./js/lib/go";
+
+export default function App() {
+  const [open, setOpen] = React.useState(false);
+  const handleHelpDrawer = () => {
+    open ? setOpen(false) : setOpen(true);
+  };
+
+  return (
+    <div className="App">
+      <Title />
+      <Diagram />
+      <Toolbar handleHelpDrawer={handleHelpDrawer} />
+      <HelpDrawer isOpen={open} />
+    </div>
+  );
+}
